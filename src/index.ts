9607b65f13b0ab7991b0d523e209afa5bc1c40c0
BCJ Dev <bcj@bcjdevelopment.com>
2022-01-27 19:04:17 -0700

    event service created

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+class EventFunnel {
+
+    private listeners: {
+        [eventName: string]: {
+            handler: (payload: any) => void,
+            id: number
+        }[]
+    } = { }
+
+    private listenerId: number = 0;
+
+    constructor() { }
+
+    publishEvent = (eventName: string, payload: any) => {
+        const listeners = this.listeners[eventName];
+        if (listeners) {
+            listeners.forEach(listener => {
+                listener.handler(payload);
+            });
+        }
+    }
+
+    createEventListener = (eventName: string, handler: (payload?: any) => void): number => {
+        this.listenerId += 1;
+        const newListenerObject = {
+            handler,
+            id: this.listenerId
+        }
+
+        if (!this.listeners[eventName]) {
+            this.listeners[eventName] = [];
+        }
+        this.listeners[eventName].push(newListenerObject);
+
+        return newListenerObject.id;
+    }
+
+    destroyEventListener = (eventName: string, listenerId: number) => {
+        if (this.listeners[eventName]) {
+            let index = this.listeners[eventName].findIndex(listener => listener.id === listenerId);
+            if (index > -1) {
+                this.listeners[eventName].splice(index, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+const eventFunnel = new EventFunnel();
+
+export const publishEvent = eventFunnel.publishEvent.bind(eventFunnel);
+export const createEventListener = eventFunnel.createEventListener.bind(eventFunnel);
+export const destroyEventListener = eventFunnel.destroyEventListener.bind(eventFunnel);
+
+export default eventFunnel;
